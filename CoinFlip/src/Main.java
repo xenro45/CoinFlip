5ca5f94f58dc91728026f968db971058b476fa9f
xenro45 <laborador7@gmail.com>
2024-08-31 10:07:45 -0500

    Fixed some scaling issues with the frame.

--- CoinFlip/src/Main.java
+++ CoinFlip/src/Main.java
@@ -5,46 +5,42 @@
     import java.util.Random;
     public class Main {
         private JFrame frame;
-        private JButton flipButton;
-        private JLabel resultLabel;
-        private ImageIcon headsIcon;
-        private ImageIcon tailsIcon;
-        private ImageIcon buttonIcon;
+        private JButton flipbutton;
+        private JLabel resultlabel;
+        private ImageIcon heads;
+        private ImageIcon tails;
+        private ImageIcon buttonicon;
         private Random random;
-        boolean heads = random.nextBoolean();
-        private JLabel statusLabel;
-        long start = System.currentTimeMillis();
 
         public Main() {
-            headsIcon = new ImageIcon(getClass().getResource("/resources/Heads.png"));
-            tailsIcon = new ImageIcon(getClass().getResource("/resources/Tails.png"));
-            buttonIcon = new ImageIcon(getClass().getResource("/resources/Button.png"));
+            heads = new ImageIcon(getClass().getResource("/resources/Heads.png"));
+            tails = new ImageIcon(getClass().getResource("/resources/Tails.png"));
+            buttonicon = new ImageIcon(getClass().getResource("/resources/Button.png"));
 
             frame = new JFrame("Coin Flip");
-            frame.setBackground(new Color(120,120,120));
-            frame.setMaximumSize(new Dimension(400, 400));
-            frame.setMinimumSize(new Dimension(400, 400));
-            frame.setForeground(new Color(120, 120,30));
+            frame.getContentPane().setBackground(new Color(179, 179, 179));
+            frame.setPreferredSize(new Dimension(500, 500));
+            frame.setMinimumSize(new Dimension(300, 300));
+            frame.setMaximumSize(new Dimension(300,300));
+            frame.setForeground(new Color(0, 0 ,0));
 
-            flipButton = new JButton(buttonIcon);
-            flipButton.setText("");
-            flipButton.setBorderPainted(false);
-            flipButton.setContentAreaFilled(false);
+            flipbutton = new JButton("");
+            flipbutton.setIcon(buttonicon);
+            flipbutton.setBorderPainted(false);
+            flipbutton.setPreferredSize(new Dimension(100, 90));
+            flipbutton.setFocusPainted(false);
 
-            statusLabel = new JLabel("Heads");
-            statusLabel.setFont(new Font("Courier", Font.CENTER_BASELINE, 50));
-            statusLabel.setBackground(new Color(120, 120,120));
-            statusLabel.setForeground(new Color(120,120,120));
+            resultlabel = new JLabel(heads);
 
-            resultLabel = new JLabel(new ImageIcon(getClass().getResource("/resources/Heads.png")));
+
             random = new Random();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setLayout(new BorderLayout());
-            frame.add(statusLabel, BorderLayout.NORTH);
-            frame.add(flipButton, BorderLayout.SOUTH);
-            frame.add(resultLabel, BorderLayout.CENTER);
+            frame.add(flipbutton, BorderLayout.SOUTH);
+            frame.add(resultlabel, BorderLayout.CENTER);
 
-            flipButton.addActionListener(new ActionListener() {
+
+            flipbutton.addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     flipCoin();
@@ -54,12 +50,12 @@
             frame.setVisible(true);
         }
 
-
         private void flipCoin() {
-            if (heads) {
-                resultLabel.setIcon(headsIcon);
+            boolean bool = random.nextBoolean();
+            if (bool) {
+                resultlabel.setIcon(heads);
             } else {
-                resultLabel.setIcon(tailsIcon);
+                resultlabel.setIcon(tails);
             }
         }
 
